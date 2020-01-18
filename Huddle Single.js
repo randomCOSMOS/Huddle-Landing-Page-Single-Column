ec706ddba6c3cee00cb3a4e4aa819337ce219c3e
RANDOM cosmos <srijanwhat2@gmail.com>
2020-01-18 15:19:49 +0530

    forced https

--- Huddle Single.js
+++ Huddle Single.js
@@ -1,7 +1,12 @@
-$("a").on('click' , function(){
+$("a").on('click', function () {
     alert("This Is A Dead Link!");
 })
 
-$(".social img").on("click", function(){
+$(".social img").on("click", function () {
     alert("Nothin Here!");
 })
+
+var loc = window.location.href + '';
+if (loc.indexOf('http://') == 0) {
+    window.location.href = loc.replace('http://', 'https://');
+}
